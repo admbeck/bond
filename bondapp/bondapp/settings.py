58de3a7d6bc2abcfeb7403d5eda530b355814542
AdmBeck <admbeck@outlook.com>
2023-02-25 22:28:33 +0500

    made all files PEP8 compliant, things might break

--- bondapp/bondapp/settings.py
+++ bondapp/bondapp/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from os import getenv
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cqjdwa5mjs)dt3j@x+!64bmszr7mc02=pn=8#ca=59o7x+h8!l'
+load_dotenv()
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'bondapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,16 +91,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        NumericPasswordValidator',
     },
 ]
 
--- bondapp/website/admin.py
+++ bondapp/website/admin.py
@@ -2,17 +2,24 @@ from django.contrib import admin
 from .models import Characters, Factions, Gallery
 from django.utils.safestring import mark_safe
 
+
 class GalleryInline(admin.TabularInline):
     """Creates inline image upload in admin panel for characters"""
     model = Gallery
     fk_name = 'character'
     extra = 1
 
+
 @admin.register(Characters)
 class CharactersAdmin(admin.ModelAdmin):
     """Creates panel for characters with automatic slugs"""
-    list_display = ('first_name', 'last_name', 'race', 'char_class', 'get_photo')
-    list_display_links = ('first_name', 'last_name')
+    list_display = ('first_name',
+                    'last_name',
+                    'race',
+                    'char_class',
+                    'get_photo')
+    list_display_links = ('first_name',
+                          'last_name')
     prepopulated_fields = {'slug': ('first_name', 'last_name')}
     list_filter = ('first_name', 'last_name')
     inlines = (GalleryInline,)
@@ -20,11 +27,13 @@ class CharactersAdmin(admin.ModelAdmin):
     def get_photo(self, obj):
         """For thumbnails"""
         if obj.images:
-            return mark_safe(f'<img src="{obj.images.all()[0].image.url}" width="75">')
+            return mark_safe(f'<img src="{obj.images.all()[0].image.url}" \
+                             width="75">')
         else:
             return '-'
 
     get_photo.short_description = 'Thumbnail'
+
 
 @admin.register(Factions)
 class FactionsAdmin(admin.ModelAdmin):
