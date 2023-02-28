fbab4c8e637526fad07757f482371ecfbc5d46d4
AdmBeck <admbeck@outlook.com>
2023-02-28 16:04:46 +0500

    created view for index page

--- bondapp/website/views.py
+++ bondapp/website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Characters
+
 
 # Create your views here.
+class MainPage(ListView):
+    """Main page"""
+    model = Characters
+    context_object_name = 'characters'
+    extra_context = {'title': 'Main page'}
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        """Output characters"""
+        characters = Characters.objects
+        return characters
